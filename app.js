62bb7dd7b81d20c3d3b71fceffe933c99b51f6e4
Wesley Souza <wesleyondo@gmail.com>
2024-11-14 19:58:45 -0300

    att com imagem

---
+++ app.js
@@ -0,0 +1,15 @@
+/**
+ * Jokenpo
+ * @author Wesley Souza
+ */
+
+function jogar() {
+    //validação da obrigatoriedade escolha do jogador
+if(document.getElementById('pedra').cheked === false &&
+document.getElementById('papel').cheked === false &&
+document.getElementById('tesoura').cheked === false) {
+}else {
+    // logica principal
+}
+
+}
