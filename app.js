63dcb189b76e52349b1042a8e815ffb7f56cbdac
Wesley Souza <wesleyondo@gmail.com>
2024-11-14 20:18:48 -0300

    att com imagem

--- app.js
+++ app.js
@@ -12,6 +12,19 @@ document.getElementById('tesoura').checked === false) {
     alert("Selecione uma opção")
 }else {
     // logica principal
+    // sorteio da opção do computador 
+    let sorteio = Math.floor(Math.random() * 3)
+    switch(sorteio) {
+        case 0:
+            document.getElementById('pc').src="img/pcpedra.png"
+            break
+        case 1:
+            document.getElementById('pc').src="img/pcpapel.png"
+            break
+        case 2:
+            document.getElementById('pc').src="img/pctesoura.png"
+            break
+    }
 }
 
 }
